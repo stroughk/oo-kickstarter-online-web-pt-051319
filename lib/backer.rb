eb987eae845a8f776f4034cca7e9ebfa2f4cca15
Kateryna Strough <katerynastrough@hotmail.com>
2019-06-07 21:13:23 +0000

    Done.

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,15 +1,16 @@
 class Backer 
   
   attr_accessor :name,  :backed_projects
-  attr_reader  :title 
+  
   
   def initialize(name)
     @name = name 
     @backed_projects = []
   end 
   
-  def back_project(Project)
-    @backed_projects << Project 
+  def back_project(project)
+    @backed_projects << project 
+    project.backers << self  
   end
   
 end
